b6b8ef9e80789af572c3aa8a6b82fb353d7f840e
deltatheprotogen7019 <126925702+acemc2978@users.noreply.github.com>
2023-11-02 19:02:59 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+result = (
+global a
+a = 0
+global b
+b = 0
+ def divider(a, b):
+     if a < b:
+         raise ValueError
+     if b > 100:
+         raise IndexError
+return a/b
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+ for key in data:
+     res = divider(key, data[key])
+     result.append(res)
+)
+ print(result)
